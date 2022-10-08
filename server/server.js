5ce4156a0a40bb1f1a6bc6b0c1106c47ff47dd20
Guy Altman <guyaltman91@gmail.com>
2022-10-08 20:58:12 +0300

    listen

---
+++ server/server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+const chalk = require("chalk");
+
+const PORT = 8181;
+app.listen(PORT, () => {
+  console.log(chalk.blueBright(`listening to port ${PORT}`));
+});
