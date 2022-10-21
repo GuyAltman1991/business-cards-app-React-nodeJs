d5f872ffdb38e457492684b409938bd235527a3b
Guy Altman <guyaltman91@gmail.com>
2022-10-21 09:45:29 +0300

    update

--- server/server.js
+++ server/server.js
@@ -2,7 +2,8 @@ const express = require("express");
 const app = express();
 const chalk = require("chalk");
 
-const PORT = 8181;
+const PORT = process.env.PORT || 8181;
 app.listen(PORT, () => {
   console.log(chalk.blueBright(`listening to port ${PORT}`));
 });
+console.log(chalk.magenta("in server.js"));
