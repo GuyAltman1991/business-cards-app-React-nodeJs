50f98fea223405abe9acb65e2bd0c367b4312350
Guy Altman <guyaltman91@gmail.com>
2022-10-29 17:02:35 +0300

    fixed

--- server/router/router.js
+++ server/router/router.js
@@ -6,5 +6,8 @@ const usersRestController = require("../users/routes/usersRestController");
 
 router.use("/cards", cardsRestController);
 router.use("/users", usersRestController);
+router.use("/", (req, res) => {
+  return res.send("base-url");
+});
 
 module.exports = router;
--- server/users/routes/usersRestController.js
+++ server/users/routes/usersRestController.js
@@ -2,7 +2,8 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  return console.log("in get");
+  console.log("in get");
+  return res.send("in get");
 });
 
 router.get("/:id", (req, res) => {
