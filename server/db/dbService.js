d255f4fd1b4b8f5a17a241c6234fe58e457aa6cf
Guy Altman <guyaltman91@gmail.com>
2022-11-16 22:24:14 +0200

    fix db

--- server/db/dbService.js
+++ server/db/dbService.js
@@ -1,9 +1,9 @@
-// const ENVIRONMENT = "dev";
-const ENVIRONMENT = "prod";
+const ENVIRONMENT = "dev";
+// const ENVIRONMENT = "prod";
 
 const connectToDB = () => {
-  if (ENVIRONMENT === "dev") require("./mongodb/connectToMongoLocally");
-  if (ENVIRONMENT === "prod") require("./mongodb/connectToAtla");
+  if (ENVIRONMENT === "dev") require("./dataBases/connectToMongoDB");
+  if (ENVIRONMENT === "prod") require("./dataBases/connectToAtlas");
 };
 
 module.exports = connectToDB;
