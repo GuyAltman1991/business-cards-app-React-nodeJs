804ff5adeabe737ffcfffbd6721dae11511bf224
GuyAltman1991 <93730542+GuyAltman1991@users.noreply.github.com>
2022-11-16 10:02:39 +0200

    Update connectToAtla.js

--- server/db/mongodb/connectToAtla.js
+++ server/db/mongodb/connectToAtla.js
@@ -3,7 +3,7 @@ const chalk = require("chalk");
 
 mongoose
   .connect(
-    "mongodb+srv://guyaltman91:itzik753@cluster0.xgdnuvm.mongodb.net/?retryWrites=true&w=majority"
+    "mongodb+srv://guyaltman91:<password>@cluster0.xgdnuvm.mongodb.net/?retryWrites=true&w=majority"
   )
   .then(() => {
     chalk.magentaBright(console.log("connected successfully to mongoDB atlas"));
